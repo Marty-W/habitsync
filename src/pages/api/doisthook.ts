2f63cfb76857f46292bf86d6492994f81aa53c5f
Martin Weber <martin.weber@01people.com>
2022-11-22 16:29:02 +0100

    refactor: add todo for streaks

--- src/pages/api/doisthook.ts
+++ src/pages/api/doisthook.ts
@@ -2,14 +2,21 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { TODOIST_EVENTS } from '../../lib/const'
 import { prisma } from '../../server/db/client'
 
+/* 
+  This fn is hit every time an user completes a task in Doist
+  It should: 
+   1) check if the task is a habit
+   2) if it is, create a timestamp for that habit
+   3) recalculate / reset current streak
+   4) update streaks
+*/
+
 const doisthook = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const { event_name, event_data } = req.body
     const habitId = event_data.id.toString()
 
-    if (event_name === TODOIST_EVENTS.ITEM_COMPLETED) {
-      // TODO batch findUnique user + habit
-
+    if (event_name === TODOIST_EVENTS.ITEM_COMPLETED && habitId) {
       try {
         await prisma.habit.findUniqueOrThrow({
           where: {
@@ -22,7 +29,6 @@ const doisthook = async (req: NextApiRequest, res: NextApiResponse) => {
             habitId: habitId,
           },
         })
-
         res.status(200).json({ messaee: 'Success' })
       } catch (e) {
         console.error(e)
