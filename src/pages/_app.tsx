1d6768f387a5d38313ff9dd0abb4e8f1108db346
Martin Weber <martin.weber@01people.com>
2022-12-19 13:06:37 +0100

    feat: ssr, bumped trpc and next ver

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 // src/pages/_app.tsx
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import type { AppType } from 'next/app'
@@ -12,6 +13,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <ReactQueryDevtools />
     </SessionProvider>
   )
 }
