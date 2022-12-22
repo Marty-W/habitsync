e9993af584bc4aa99dca5c37624e0af0e3bc50f4
Martin Weber <martin.weber@01people.com>
2022-12-22 20:23:00 +0100

    feat: detail temp icons, link to todoist

--- src/pages/habits/[id].tsx
+++ src/pages/habits/[id].tsx
@@ -7,60 +7,69 @@ import Calendar from '../../components/calendar/calendar'
 import CardSkeleton from '../../components/cardSkeleton'
 import HabitDescription from '../../components/habitDescription'
 // import Streaks from '../../components/streaks'
+import { HiOutlineCog } from 'react-icons/hi'
+import { SiTodoist } from 'react-icons/si'
 import { trpc } from '../../lib/trpc'
 
 const HabitDetail = () => {
-    const id = useRouter().query.id as string
-    const name = useRouter().query.name as string
-    const timestamps = trpc.timestamp.getAll.useQuery({
-        habitId: id,
-    })
-    const description = trpc.habit.getDetail.useQuery({
-        id: id,
-    })
+  const id = useRouter().query.id as string
+  const name = useRouter().query.name as string
+  const timestamps = trpc.timestamp.getAll.useQuery({
+    habitId: id,
+  })
+  const description = trpc.habit.getDetail.useQuery({
+    id: id,
+  })
 
-    if (description.isError || timestamps.isError) {
-        const statusCode =
-            (description.error?.data?.httpStatus ?? 500) ||
-            (timestamps.error?.data?.httpStatus ?? 500)
+  if (description.isError || timestamps.isError) {
+    const statusCode =
+      (description.error?.data?.httpStatus ?? 500) ||
+      (timestamps.error?.data?.httpStatus ?? 500)
 
-        const title = description.error?.message || timestamps.error?.message
+    const title = description.error?.message || timestamps.error?.message
 
-        return <NextError statusCode={statusCode} title={title} />
-    }
+    return <NextError statusCode={statusCode} title={title} />
+  }
 
-
-    return (
-        <div className='flex min-h-screen flex-col bg-slate-200 p-5'>
-            <div className='grid grid-cols-3 items-center text-center'>
-                <motion.button whileTap={{ scale: 0.95 }}>
-                    <Link href='/habits'>
-                        <BsArrowLeft size='1.5rem' className='text-zinc-500' />
-                    </Link>
-                </motion.button>
-                <div>
-                    <h1 className='ml-auto justify-self-center text-xl text-zinc-800'>
-                        {name}
-                    </h1>
-                </div>
-            </div>
-            {description.isSuccess ? (
-                <HabitDescription desc={description.data} />
-            ) : (
-                <CardSkeleton count={5} />
-            )}
-            {timestamps.isSuccess ? (
-                <Calendar timestamps={timestamps.data} />
-            ) : (
-                <CardSkeleton count={6} />
-            )}
-            {/* {bestStreaks && (
+  return (
+    <div className='flex min-h-screen flex-col bg-slate-200 p-5'>
+      <div className='mb-5 grid grid-cols-3 items-center text-center'>
+        <motion.button whileTap={{ scale: 0.95 }}>
+          <Link href='/habits'>
+            <BsArrowLeft size='1.5rem' className='text-zinc-500' />
+          </Link>
+        </motion.button>
+        <div>
+          <h1 className='ml-auto justify-self-center text-xl text-zinc-800'>
+            {name}
+          </h1>
+        </div>
+        <div className='flex items-center justify-end'>
+          <Link href={`${description.data?.url}`} className='mr-2 '>
+            <SiTodoist size='1.5rem' className='text-zinc-500' />
+          </Link>
+          <Link href={``}>
+            <HiOutlineCog size='1.5rem' className='text-zinc-500' />
+          </Link>
+        </div>
+      </div>
+      {description.isSuccess ? (
+        <HabitDescription desc={description.data} />
+      ) : (
+        <CardSkeleton count={5} />
+      )}
+      {timestamps.isSuccess ? (
+        <Calendar timestamps={timestamps.data} />
+      ) : (
+        <CardSkeleton count={6} />
+      )}
+      {/* {bestStreaks && (
         <div>
           <Streaks streaks={bestStreaks} />
         </div>
       )}{' '} */}
-        </div>
-    )
+    </div>
+  )
 }
 
 export default HabitDetail
