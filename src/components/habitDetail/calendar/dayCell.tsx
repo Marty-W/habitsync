0fbdc6fd4c58f6dd43f2d0659ed9d81b44cb8a55
Martin Weber <martin.weber@01people.com>
2023-01-03 11:49:36 +0100

    fix: redesign dayCell states

--- src/components/habitDetail/calendar/dayCell.tsx
+++ src/components/habitDetail/calendar/dayCell.tsx
@@ -7,17 +7,18 @@ interface Props {
   date: number
 }
 
+//FIX dnesek neni dvojbarevny (cerna-cerna, x -x)
+
 const DayCell = ({ date, hasTimestamp, isToday, isThisMonth }: Props) => {
   return (
     <div
       className={clsx(
-        'flex h-10 w-10 items-center justify-center rounded-2xl p-2 text-lg text-zinc-600',
+        'm-1 flex h-9 w-9 items-center justify-center rounded-full p-3 text-lg text-zinc-600',
         {
-          'border-2 border-blue-700': isToday,
+          'border-2 border-red-500': isToday,
           'text-zinc-200': !isThisMonth,
-          'bg-slate-50': isThisMonth,
           'text-red-200': hasTimestamp,
-          'text-slate-0 bg-red-500': hasTimestamp && isThisMonth,
+          'text-red-500': hasTimestamp && isThisMonth,
         }
       )}
     >
