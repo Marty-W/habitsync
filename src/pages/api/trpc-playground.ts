0c3e4b37511400a8a3b3329cc90cd96c648a6041
Martin Weber <martin8weber@gmail.com>
2023-01-09 10:54:35 +0100

    feat(server): add trpc-playground pckg

---
+++ src/pages/api/trpc-playground.ts
@@ -0,0 +1,20 @@
+// pages/api/trpc-playground.ts
+import { NextApiHandler } from 'next'
+import { appRouter } from 'server/trpc/router'
+import { nextHandler } from 'trpc-playground/handlers/next'
+
+const setupHandler = nextHandler({
+  router: appRouter,
+  trpcApiEndpoint: '/api/trpc',
+  playgroundEndpoint: '/api/trpc-playground',
+  request: {
+    superjson: true,
+  },
+})
+
+const handler: NextApiHandler = async (req, res) => {
+  const playgroundHandler = await setupHandler
+  await playgroundHandler(req, res)
+}
+
+export default handler
