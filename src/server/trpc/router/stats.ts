c3ba16773dffe89a94d55c6163390d30fc0cbbba
Martin Weber <martin8weber@gmail.com>
2023-01-27 12:23:36 +0100

    fix: fix old methods (failing build again)

--- src/server/trpc/router/stats.ts
+++ src/server/trpc/router/stats.ts
@@ -1,7 +1,7 @@
 import {
-  getNumberOfDaysInInterval,
-  getNumberOfTimestampsInInterval,
-  getSuccessRate,
+    getNumberOfDaysInInterval,
+    getNumberOfTimestampsInInterval,
+    getSuccessRate,
 } from '@/server/common/recurrence'
 import { TRPCError } from '@trpc/server'
 import { startOfDay } from 'date-fns'
@@ -11,63 +11,64 @@ import { authedProcedure, t } from '../trpc'
 // TODO Check if there is a possibility to make sure these are in date format (or simply use date?)
 
 export const statsRouter = t.router({
-  getTotalHabitCompletions: authedProcedure
-    .input(z.object({ habitId: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const { habitId } = input
+    getTotalHabitCompletions: authedProcedure
+        .input(z.object({ habitId: z.string() }))
+        .query(async ({ ctx, input }) => {
+            const { habitId } = input
 
-      return await ctx.prisma.timestamp.count({
-        where: {
-          habitId,
-        },
-      })
-    }),
-  getHabitSuccessRate: authedProcedure
-    .input(
-      z.object({
-        habitId: z.string(),
-        startDate: z.string().optional(),
-        endDate: z.string().optional(),
-      }),
-    )
-    .query(async ({ ctx, input }) => {
-      const { habitId } = input
+            return await ctx.prisma.timestamp.count({
+                where: {
+                    habitId,
+                },
+            })
+        }),
+    getHabitSuccessRate: authedProcedure
+        .input(
+            z.object({
+                habitId: z.string(),
+                startDate: z.string().optional(),
+                endDate: z.string().optional(),
+            }),
+        )
+        .query(async ({ ctx, input }) => {
+            const { habitId } = input
 
-      const habit = await ctx.prisma.habit.findUnique({
-        where: {
-          id: habitId,
-        },
-        select: {
-          timestamps: true,
-          createdAt: true,
-          recurrenceType: true,
-          recurrenceStep: true,
-          recurrenceDays: true,
-        },
-      })
+            const habit = await ctx.prisma.habit.findUnique({
+                where: {
+                    id: habitId,
+                },
+                select: {
+                    timestamps: true,
+                    createdAt: true,
+                    recurrenceType: true,
+                    recurrenceStep: true,
+                    recurrenceDays: true,
+                },
+            })
 
-      if (!habit) {
-        throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: 'Habit not found',
-        })
-      }
+            if (!habit) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: 'Habit not found',
+                })
+            }
 
-      const interval = {
-        start: input.startDate
-          ? startOfDay(new Date(input.startDate))
-          : startOfDay(habit.createdAt),
-        end: input.endDate ? startOfDay(new Date(input.endDate)) : startOfDay(new Date()),
-      }
+            const interval = {
+                start: input.startDate
+                    ? startOfDay(new Date(input.startDate))
+                    : startOfDay(habit.createdAt),
+                end: input.endDate ? startOfDay(new Date(input.endDate)) : startOfDay(new Date()),
+            }
 
-      const numOfDaysInInterval = getNumberOfDaysInInterval(interval, habit.recurrenceType, {
-        step: habit.recurrenceStep || undefined,
-        days: habit.recurrenceDays as Weekday[],
-      })
-      //FIX rewrite the type casts
+            //FIX rewrite the type casts
+            const numOfDaysInInterval = getNumberOfDaysInInterval(interval, {
+                days: habit.recurrenceDays as Weekday[],
+                step: habit.recurrenceStep!,
+                type: habit.recurrenceType,
+            })
 
-      const numOfTimestampsInInterval = getNumberOfTimestampsInInterval(habit.timestamps, interval)
+            const numOfTimestampsInInterval = getNumberOfTimestampsInInterval(habit.timestamps, interval)
 
-      return getSuccessRate(numOfTimestampsInInterval, numOfDaysInInterval)
-    }),
+            return getSuccessRate(numOfTimestampsInInterval, numOfDaysInInterval)
+        }),
 })
--- src/server/trpc/router/streak.ts
+++ src/server/trpc/router/streak.ts
@@ -5,87 +5,87 @@ import { TRPCError } from '@trpc/server'
 import { Weekday } from 'types'
 
 export const streakRouter = t.router({
-  getCurrent: authedProcedure
-    .input(z.object({ habitId: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const { habitId } = input
+    getCurrent: authedProcedure
+        .input(z.object({ habitId: z.string() }))
+        .query(async ({ ctx, input }) => {
+            const { habitId } = input
 
-      const habit = await ctx.prisma.habit.findUnique({
-        where: {
-          id: habitId,
-        },
-        include: {
-          timestamps: {
-            orderBy: {
-              time: 'desc',
-            },
-            select: {
-              time: true,
-            },
-          },
-        },
-      })
+            const habit = await ctx.prisma.habit.findUnique({
+                where: {
+                    id: habitId,
+                },
+                include: {
+                    timestamps: {
+                        orderBy: {
+                            time: 'desc',
+                        },
+                        select: {
+                            time: true,
+                        },
+                    },
+                },
+            })
 
-      if (!habit) {
-        throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: 'Habit not found',
-        })
-      }
+            if (!habit) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: 'Habit not found',
+                })
+            }
 
-      return calculateCurrentStreak(habit.timestamps.map(timestamp => timestamp.time))
-    }),
-  getBest: authedProcedure
-    .input(z.object({ habitId: z.string(), numStreaks: z.number() }))
-    .query(async ({ ctx, input }) => {
-      const { habitId } = input
+            return calculateCurrentStreak(habit.timestamps.map(timestamp => timestamp.time))
+        }),
+    getBest: authedProcedure
+        .input(z.object({ habitId: z.string(), numStreaks: z.number() }))
+        .query(async ({ ctx, input }) => {
+            const { habitId } = input
 
-      const habit = await ctx.prisma.habit.findUnique({
-        where: {
-          id: habitId,
-        },
-        select: {
-          timestamps: {
-            orderBy: {
-              time: 'desc',
-            },
-            select: {
-              time: true,
-            },
-          },
-          recurrenceDays: true,
-          recurrenceStep: true,
-          recurrenceType: true,
-        },
-      })
+            const habit = await ctx.prisma.habit.findUnique({
+                where: {
+                    id: habitId,
+                },
+                select: {
+                    timestamps: {
+                        orderBy: {
+                            time: 'desc',
+                        },
+                        select: {
+                            time: true,
+                        },
+                    },
+                    recurrenceDays: true,
+                    recurrenceStep: true,
+                    recurrenceType: true,
+                },
+            })
 
-      if (!habit || !habit.recurrenceType) {
-        throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: 'Habit not found',
-        })
-      }
-      if (habit.recurrenceType === 'every_x_days' && !habit.recurrenceStep) {
-        throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: 'Habit is set to repeat every x days, but no recurrence step is set',
-        })
-      }
-      if (habit.recurrenceType === 'specific_days' && !habit.recurrenceDays) {
-        throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: 'Habit is set to repeat on specific days, but they are no set',
-        })
-      }
+            if (!habit || !habit.recurrenceType) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: 'Habit not found',
+                })
+            }
+            if (habit.recurrenceType === 'every_x_days' && !habit.recurrenceStep) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: 'Habit is set to repeat every x days, but no recurrence step is set',
+                })
+            }
+            if (habit.recurrenceType === 'specific_days' && !habit.recurrenceDays) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: 'Habit is set to repeat on specific days, but they are no set',
+                })
+            }
 
-      return calculateAllStreaks(
-        habit.timestamps.map(timestamp => timestamp.time),
-        habit.recurrenceType,
-        false,
-        {
-          days: habit.recurrenceDays as Weekday[],
-          step: habit.recurrenceStep as number,
-        },
-      ).slice(0, input.numStreaks)
-    }),
+            //FIX rewrite the type casts
+            return calculateAllStreaks(
+                habit.timestamps.map(timestamp => timestamp.time),
+                {
+                    type: habit.recurrenceType,
+                    days: habit.recurrenceDays as Weekday[],
+                    step: habit.recurrenceStep!,
+                },
+            ).slice(0, input.numStreaks)
+        }),
 })
