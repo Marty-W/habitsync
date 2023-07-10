f1e58ec62cb24380b548e7649b7a6c78e7d741b4
Martin Weber <53663335+Marty-W@users.noreply.github.com>
2023-07-10 18:35:10 +0200

    feat: add more theme options

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -34,6 +34,14 @@ export default {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))",
+        },
+        void: {
+          DEFAULT: "hsl(var(--void))",
+          foreground: "hsl(var(--success-foreground))",
+        },
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
