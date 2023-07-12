ae6d3512ae0d46ed1c3dc6685d95120df7dcf319
Martin Weber <53663335+Marty-W@users.noreply.github.com>
2023-07-13 01:19:11 +0200

    [WIP]feat: create auth section

---
+++ apps/nextjs/src/components/settings/deleteAccountItem.tsx
@@ -0,0 +1,37 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "~/components/ui/alertDialog"
+import SettingsItem from "./settingsItem"
+
+const DeleteAccountItem = () => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <SettingsItem title="Delete account" as="button" />
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
+
+export default DeleteAccountItem
