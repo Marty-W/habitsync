3087466f8ea2b5992f1345c3ebeb8c52621ac947
Martin Weber <53663335+Marty-W@users.noreply.github.com>
2023-08-07 09:10:51 +0200

    fix: convert _app.tsx to appdir

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,20 +1,23 @@
 import { Suspense } from "react"
+import Link from "next/link"
+
 import { auth } from "@habitsync/auth"
+
+import { SignIn, SignOut } from "~/components/auth"
 
 export default function HomePage() {
   return (
     <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 px-4 py-8">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-pink-400">T3</span> Turbo
-        </h1>
-        <AuthShowcase />
-
-        {/* <CreatePostForm /> */}
-        <Suspense fallback={<span>Loading...</span>}>
-          <span>test</span>
-          {/* <PostList /> */}
-        </Suspense>
+        <div className="min-h-screen">
+          <h1 className="text-5xl font-bold">HabitSync</h1>
+          <p>Use todoist for tracking your habits.</p>
+          <p>I get it. You love todoist. You also want to track your habits.</p>
+          <AuthShowcase />
+          <Suspense fallback={<span>Loading...</span>}>
+            <span>test</span>
+          </Suspense>
+        </div>
       </div>
     </main>
   )
@@ -24,16 +27,25 @@ export async function AuthShowcase() {
   const session = await auth()
 
   if (!session) {
-    return <span>Sign in with Discord</span>
+    return (
+      <SignIn
+        provider="google"
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+      >
+        Sign in with Google
+      </SignIn>
+    )
   }
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {session && <span>Logged in as {session.user.name}</span>}
-      </p>
+    <>
+      <Link className="btn" href="/habits">
+        Go to dashboard
+      </Link>
 
-      <span>Sign out</span>
-    </div>
+      <SignOut className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
+        Sign out
+      </SignOut>
+    </>
   )
 }
